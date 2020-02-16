8e7720880f8837c146fd7d7a0f6e6af54992dead
norihirosunada <norisuna1201@gmail.com>
2020-02-16 13:49:52 +0000

    done

--- app.rb
+++ app.rb
@@ -6,14 +6,33 @@ require 'open-uri'
 require 'net/http'
 require 'json'
 
-get '/' do 
+require './models'
+
+get '/' do
+    erb :index
+end
+
+get '/search' do 
     keyword = params[:keyword]
     uri = URI("https://itunes.apple.com/search")
     uri.query = URI.encode_www_form({ term: keyword, country: "US", media: "music", limit: 10})
     res = Net::HTTP.get_response(uri)
     returned_json = JSON.parse(res.body)
-    @musics = returned_json["results"]
 
-    erb :index
+    returned_json["results"].map! do |music|
+        music_info = Music.find_or_create_by(track_id: music["trackId"])
+        music["id"] = music_info.id
+        music["like"] = music_info.like
+        music
+    end
+
+    json returned_json["results"]
 end
 
+post '/musics/:id/like' do 
+    music = Music.find(params[:id])
+    music.like = music.like + 1
+    music.save
+
+    music.like.to_s
+end
